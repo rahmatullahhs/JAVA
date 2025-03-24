6ead472e7373865f8eb5d9f4f6dd556591b2d225
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-03-24 15:15:09 +0600

    add

--- TestDataType/src/testdatatype/TestDataType.java
+++ TestDataType/src/testdatatype/TestDataType.java
@@ -6,6 +6,7 @@ package testdatatype;
 
 import java.util.Scanner;
 
+
 /**
  *
  * @author Admin
@@ -18,11 +19,47 @@ public class TestDataType {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("enter name");
-        String name = input.nextLine();
-        System.out.println("My name " + name);
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("enter name");
+//        String name = input.nextLine();
+//        System.out.println("My name " + name);
+        
 
+
+
+   
+   
+        byte id= 120;
+        short roll= 12342;
+        int number= 21454656;
+        long longNumber= 124325465768798789l;
+        float price= 1232.345f;
+        double doublePrice= 1234.2134;
+        
+        char letter= '\u0041';
+        boolean status= true;
+        
+        System.out.println("The letter is "+ letter);
+        String name ="sathe";
+        System.out.println("His name is "+ name);
+        System.out.println("This status is "+ status);
+        System.out.println("This status is "+ price);
+        System.out.println("This status is "+ doublePrice);
+        System.out.println("This status is "+ longNumber);
+        System.out.println("This status is "+ roll);
+        System.out.println("This status is "+ id);
+        System.out.println("This status is "+ number);
+        
+        
+        
+        
+        
+        
+        
     }
-
+    
+  
 }
+    
+
+
