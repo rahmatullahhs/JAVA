a66e11bb8bce3bbdfc03c053a1e57d2e69aa349c
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-05 18:14:31 +0600

    add

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -15,20 +15,25 @@ public class JavaApplication9 {
      */
     public static void main(String[] args) {
         // TODO code application logic here        // TODO code application logic here
-  
-    System.out.println("Hello World");
-    
-int i=10;
-int b=i++;
-  System.out.println(i++);
-        System.out.println(b++);
-         System.out.println(i--);
-         System.out.println(++i);
-         System.out.println(--i);
-         
-         System.out.println((double)1/2);
-        System.out.println(1/2);
+//  
+//    System.out.println("Hello World");
+//    
+//int i=10;
+//int b=i++;
+//  System.out.println(i++);
+//        System.out.println(b++);
+//         System.out.println(i--);
+//         System.out.println(++i);
+//         System.out.println(--i);
+//         
+//         System.out.println((double)1/2);
+//        System.out.println(1/2);
+
+
         
+
+
+
         
         
         
---
+++ loan/src/loan/Loan.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Loan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter interest rate as 12.5%");
+        double interestRate=s.nextDouble();
+        double monthlyInterestRate=interestRate/1200;
+        
+        System.out.println("enter number of year");
+        int numberOfYear=s.nextInt();
+        
+        System.out.println("Enter loan amount");
+        double LoanAmount=s.nextDouble();
+        
+        double monthlyPayment = LoanAmount * monthlyInterestRate /(1
+                -1/Math.pow(1+ monthlyInterestRate, numberOfYear * 12));
+        
+        double totalPayment=monthlyPayment*numberOfYear*12;
+        
+         System.out.println("The monthly payment is $" + (int) monthlyPayment );
+       System.out.println("The total payment is $" + (int)totalPayment);
+        
+    }
+    
+}
