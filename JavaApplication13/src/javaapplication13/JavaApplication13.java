0206b09018b2113dc4743771492dbcedd1eb32e3
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-07 15:40:32 +0600

    add

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -4,6 +4,8 @@
  */
 package javaapplication13;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -15,6 +17,35 @@ public class JavaApplication13 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+           Scanner s = new Scanner(System.in);
+         System.out.println("Enter a year to check Leap Year");
+     int year = s.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " is Leap Year");
+                } else {
+                    System.out.println(year + " not Leap Year");
+                }
+
+            } else {
+                System.out.println(year + " Leap Year");
+            }
+        } else {
+            System.out.println(year + " is not Leap Year");
+        }
+    }
+
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
-}
+
