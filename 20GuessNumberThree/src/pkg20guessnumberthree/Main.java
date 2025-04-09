a89f24c3f2de3703607ca4a23fc8b570e86ac812
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-09 16:31:53 +0600

    add

--- 20GuessNumberThree/src/pkg20guessnumberthree/Main.java
+++ 20GuessNumberThree/src/pkg20guessnumberthree/Main.java
@@ -1,20 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg20guessnumberthree;
 
-/**
- *
- * @author Admin
- */
+import java.util.Scanner;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner s = new Scanner(System.in);
+        int randomNumber = (int) (Math.random() * 10);
+        int userInput = s.nextInt();
+        int count = 1;
+        while (count < 3) {
+
+            if (randomNumber == userInput) {
+                System.out.println("win");
+                 break;
+            }
+           
+        
+        else if(randomNumber < userInput){
+                System.out.println("try again");
+                
+                 userInput=s.nextInt();
+                }
+        
+        else if(randomNumber > userInput){
+                System.out.println("try again");
+                
+                 userInput=s.nextInt();
+                }
+
+        if (count == 2) {
+            System.out.println("lose");
+        }
+
+        count++;
+}
+
+        
+        
+        
+        
+        
+        
+        
+        
     }
-    
+
 }
