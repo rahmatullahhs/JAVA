267c5fa55451578d02f6ef9f80ea22d49ec93b1e
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-09 18:54:53 +0600

    add

--- ForLoop/src/forloop/ForLoop.java
+++ ForLoop/src/forloop/ForLoop.java
@@ -1,5 +1,6 @@
 package forloop;
 
+import com.sun.source.tree.ContinueTree;
 import java.util.Scanner;
 
 public class ForLoop {
@@ -8,22 +9,56 @@ public class ForLoop {
 
         Scanner s = new Scanner(System.in);
 
-        System.out.println("enter number");
-        int input = s.nextInt();
-        int count = 0;
+//        System.out.println("enter number");
+//        int input = s.nextInt();
+//        int count = 0;
+//
+//        for (int i = 1; i <= input; i++) {
+//            if (input % i == 0) {
+//                count++;
+//            }
+//        }
+//
+//        if (count == 2) {
+//            System.out.println(input + " prime");
+//        } else {
+//            System.out.println("not prime");
+//        }
 
-        for (int i = 1; i <= input; i++) {
-            if (input % i == 0) {
-                count++;
-            }
+//        int targetValue=50;
+//        int counter   =1;
+//        
+//        while (counter<=targetValue) {
+//            if (counter%3==0 && counter%5==0) {
+//                
+//                counter++;
+//                continue;
+//         }
+//            System.out.println(counter);
+//            counter++;
+//            
+//        }
+        
+        
+//        int counter = 0;
+//          int targetValue=5;
+//          
+//          while (counter<targetValue) { 
+//              
+//              if (counter<=targetValue) {
+//                  counter++;
+//                  System.out.println(counter);
+//              }
+//              
+//                   
+//        }  counter++;
+       long userinput=s.nextLong();
+        for(long i = 1; i <=500000000 ; i++){
+            System.out.println(i + "You have been hacked");
         }
-
-        if (count == 2) {
-            System.out.println(input + " prime");
-        } else {
-            System.out.println("not prime");
-        }
-
+        
+    
+        
     }
 
 }
