bd6e9236ecf19dc65b77666d87ffc53325f7d3cb
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-13 16:57:56 +0600

    add

--- arrayPractice/src/arraypractice/ArrayPractice.java
+++ arrayPractice/src/arraypractice/ArrayPractice.java
@@ -9,31 +9,48 @@ public class ArrayPractice {
     public static void main(String[] args) {
         
         
-        String[] names=new String[14];
+//        String[] names=new String[14];
+//        
+//        int[]marks=new int[14];
+//        
+//        int[] ages={10,25,98,45,25,44};
+//        
+//        System.out.println(Arrays.toString(ages));
+//        
+//        names[0]="rahmat";
+//        
+//        System.out.println(Arrays.toString(names));
+//        
+//       ------------------------- 
         
-        int[]marks=new int[14];
-        
-        int[] ages={10,25,98,45,25,44};
-        
-        System.out.println(Arrays.toString(ages));
-        
-        names[0]="rahmat";
-        
-        System.out.println(Arrays.toString(names));
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+         
+      String[] names={"Rahmat","Easin","Parvaz","Rakib","sadier"};
+      
+        int[]marks={44,55,66,77,88,99};
+//        
+//        System.out.println("My name is " + Arrays.toString(names).concat(Arrays.toString(marks)));
+//        
+//        String[] list= new String[5];
+//         list[0]="my name is rahmat and mark 22";
+//          list[1]="my name is Easin and mark 22";
+//           list[2]="my name is Parvaz and mark 22";
+//            list[3]="my name is Rakib and mark 22";
+//         list[4]="my name is sadier and mark 22";
+//        
+//         System.out.println(Arrays.toString(list)+ "\n");
+         
+         
+         for(int index=0; index<names.length; index++){
+             System.out.println("My name is " + names[index]+ "mark " +marks[index] );
+         
+         }
+         
+         
+         
+         
+         
+         
+         
         
 //        -----------
     }
