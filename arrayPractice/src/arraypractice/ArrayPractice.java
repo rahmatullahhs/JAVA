df5ddd752eed65b90a6fdcc0cd95e62409fd1273
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-13 16:14:14 +0600

    add

---
+++ arrayPractice/src/arraypractice/ArrayPractice.java
@@ -0,0 +1,64 @@
+
+package arraypractice;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+
+
+    public static void main(String[] args) {
+        
+        
+        String[] names=new String[14];
+        
+        int[]marks=new int[14];
+        
+        int[] ages={10,25,98,45,25,44};
+        
+        System.out.println(Arrays.toString(ages));
+        
+        names[0]="rahmat";
+        
+        System.out.println(Arrays.toString(names));
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        -----------
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//   --------------- 
+}
