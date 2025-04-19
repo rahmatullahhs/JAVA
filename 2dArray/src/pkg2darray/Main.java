7b7a26981fa477cc7090954bec34e3ad8dfabed2
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-19 15:19:05 +0600

    add

---
+++ 2dArray/src/pkg2darray/Main.java
@@ -0,0 +1,53 @@
+
+package pkg2darray;
+
+import java.util.Arrays;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int[][] userArrays=new int[3][3];       
+ 
+        userArrays[0][0]=10;
+        userArrays[0][1]=50;
+        userArrays[0][2]=30;
+        
+        userArrays[1][0]=11;
+        userArrays[1][1]=40;
+        userArrays[1][2]=60;
+        
+         userArrays[2][0]= 12;
+        userArrays[2][1]=33;
+        userArrays[2][2]=22;
+      
+       
+        System.out.println(Arrays.deepToString(userArrays));
+        
+        
+        }
+        
+    }
+    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
