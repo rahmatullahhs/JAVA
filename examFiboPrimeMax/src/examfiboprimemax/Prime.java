c7588b3242594deaf1c5837a6778b0b0aeb1aefe
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-20 18:53:40 +0600

    add

---
+++ examFiboPrimeMax/src/examfiboprimemax/Prime.java
@@ -0,0 +1,33 @@
+
+package examfiboprimemax;
+
+import java.util.Scanner;
+
+
+public class Prime {
+ 
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("input number: ");
+        int input = s.nextInt();
+
+        int round = 0;
+
+        for (int i = 1; i <= input; i++) {
+            if (input % i == 0) {
+                round++;
+            }
+        }
+
+        if (round == 2) {
+            System.out.println(input + " prime.");
+        } else {
+            System.out.println(input + " not prime.");
+        }
+      
+     
+    }
+
+}
