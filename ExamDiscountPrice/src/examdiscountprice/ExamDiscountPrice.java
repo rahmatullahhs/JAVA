d7f38c5ab22a469c8158c51a0e685fd9ea818bbb
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-21 18:24:38 +0600

    add

--- ExamDiscountPrice/src/examdiscountprice/ExamDiscountPrice.java
+++ ExamDiscountPrice/src/examdiscountprice/ExamDiscountPrice.java
@@ -1,14 +1,25 @@
 
 package examdiscountprice;
 
+import java.util.Scanner;
+
 
 public class ExamDiscountPrice {
 
     public static void main(String[] args) {
        
+        Scanner s=new Scanner(System.in);
         
+        double userPay=s.nextDouble();
+     double discountPrice = userPay -(userPay * 0.05);
+        if (userPay > 200 ) {
         
-        
+          System.out.println("Actual price :" + userPay +" Discount price:" + discountPrice);
+        }
+        else{  System.out.println("Actual price :" + userPay +" no price: 00");
+        }
+      
+     
         
     }
     
