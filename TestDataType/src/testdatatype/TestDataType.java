0199e5c0e9bec461fed910ac044f85cd5644a787
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-22 15:50:56 +0600

    add

--- TestDataType/src/testdatatype/TestDataType.java
+++ TestDataType/src/testdatatype/TestDataType.java
@@ -1,54 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package testdatatype;
 
 import java.util.Scanner;
 
 
-/**
- *
- * @author Admin
- */
+
 public class TestDataType {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
 
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("enter name");
-//        String name = input.nextLine();
-//        System.out.println("My name " + name);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter name");
+        String name = input.nextLine();
+        System.out.println("My name is " + name);
         
 
 
 
    
    
-        byte id= 120;
-        short roll= 12342;
-        int number= 21454656;
-        long longNumber= 124325465768798789l;
-        float price= 1232.345f;
-        double doublePrice= 1234.2134;
-        
-        char letter= '\u0041';
-        boolean status= true;
-        
-        System.out.println("The letter is "+ letter);
-        String name ="sathe";
-        System.out.println("His name is "+ name);
-        System.out.println("This status is "+ status);
-        System.out.println("This status is "+ price);
-        System.out.println("This status is "+ doublePrice);
-        System.out.println("This status is "+ longNumber);
-        System.out.println("This status is "+ roll);
-        System.out.println("This status is "+ id);
-        System.out.println("This status is "+ number);
+//        byte id= 120;
+//        short roll= 12342;
+//        int number= 21454656;
+//        long longNumber= 124325465768798789l;
+//        float price= 1232.345f;
+//        double doublePrice= 1234.2134;
+//        
+//        char letter= '\u0041';
+//        boolean status= true;
+//        
+//        System.out.println("The letter is "+ letter);
+//        String name ="sathe";
+//        System.out.println("His name is "+ name);
+//        System.out.println("This status is "+ status);
+//        System.out.println("This status is "+ price);
+//        System.out.println("This status is "+ doublePrice);
+//        System.out.println("This status is "+ longNumber);
+//        System.out.println("This status is "+ roll);
+//        System.out.println("This status is "+ id);
+//        System.out.println("This status is "+ number);
         
         
         
