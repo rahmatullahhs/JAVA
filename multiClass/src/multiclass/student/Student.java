0ca32b84501cb9c693e47ecf068159bff36a1eca
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-22 16:41:59 +0600

    add

---
+++ multiClass/src/multiclass/student/Student.java
@@ -0,0 +1,78 @@
+
+package multiclass.student;
+
+
+public class Student {
+    
+   private String name;
+   private  String email;
+    private  int id;
+    private  double contact;
+    private  String Address;
+
+    public Student() {
+    }
+
+    public Student(String name, String email, int id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+
+    public Student(String name, String email, int id, double contact, String Address) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.contact = contact;
+        this.Address = Address;
+    }
+    
+    
+    public String getName(){
+    return name;
+    }
+    
+    public void setName(){
+        if (name.length()>=6 && name.length()<=10) {
+            this.name = name;
+        }
+  
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getContact() {
+        return contact;
+    }
+
+    public void setContact(double contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+    
+    
+    
+    
+    
+}
