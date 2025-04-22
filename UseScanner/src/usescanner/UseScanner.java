a2f235cf4e1093158daa708fb649a0567129d821
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-22 15:57:24 +0600

    add

--- UseScanner/src/usescanner/UseScanner.java
+++ UseScanner/src/usescanner/UseScanner.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package usescanner;
 
 import java.util.Scanner;
@@ -12,23 +9,12 @@ public class UseScanner {
    
     public static void main(String[] args) {
     
-       
-    
-        Scanner s=new Scanner(System.in);
-        
-//        System.out.print("enter 1st number");
-//        int number1=input.nextInt();
-//           System.out.print("enter 2st number");
-//        int number2=input.nextInt();
-//        
-//        int result=number1+number2;
-//                   System.out.print(result);
-
+  Scanner s=new Scanner(System.in);
+//            number genarator
         int input=s.nextInt();
-       for (int i=1; i<=input ; i++ ){
-        
+     
+        for (int i=1; i<=input ; i++ ){
         System.out.println(i);
-        
         }
         
         
