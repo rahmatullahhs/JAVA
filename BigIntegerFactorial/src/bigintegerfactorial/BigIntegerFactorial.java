e4ae4b1f39b380384b0f4b0c248e08060769fbaf
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-23 16:27:43 +0600

    add

--- BigIntegerFactorial/src/bigintegerfactorial/BigIntegerFactorial.java
+++ BigIntegerFactorial/src/bigintegerfactorial/BigIntegerFactorial.java
@@ -1,33 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package bigintegerfactorial;
 
 import java.math.BigInteger;
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
 public class BigIntegerFactorial {
 
     public static void main(String[] args) {
-
+//take a variable of scaner
         Scanner s = new Scanner(System.in);
-
+//print what to do a user
         System.out.println("Enter Number");
-
+//a storage for data which user inputed
         int userInput = s.nextInt();
+//        make a variable "fctorial" in BigInteger datatype
+
 //        BigInteger factorial= new BigInteger("10");
 //      BigInteger factorial= BigInteger.valueOf("10");
         BigInteger factorial = BigInteger.ONE;
-
+//for loop for math
         for (int i = 1; i <= userInput; i++) {
-
+//variable assined as result carrier
             factorial = factorial.multiply(BigInteger.valueOf(i));
         }
+//        print result
         System.out.println(factorial);
 
         
