42482c6577eb87e48d3f77f08c638e2710c50112
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-23 16:03:27 +0600

    add

---
+++ BigIntegerFactorial/src/bigintegerfactorial/BigIntegerFactorial.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bigintegerfactorial;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class BigIntegerFactorial {
+
+ 
+    
+    public static void main(String[] args) {
+       
+        
+ Scanner s =new Scanner(System.in);
+ 
+       
+        System.out.println("limit");
+       
+       int userInput=s.nextInt();
+        
+     
+        BigInteger factorial=BigInteger.ONE;
+        
+         for(int i = 1; i <= userInput; i++){
+             
+   factorial= factorial.multiply(BigInteger.valueOf(i));
+
+         }
+
+        System.out.println(factorial);
+
+
+        
+    }
+    
+}
