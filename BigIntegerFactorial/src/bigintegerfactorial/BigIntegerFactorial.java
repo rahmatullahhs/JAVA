fd012ba2888769734167adab86f2aeebf133c3db
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-23 16:10:17 +0600

    add

--- BigIntegerFactorial/src/bigintegerfactorial/BigIntegerFactorial.java
+++ BigIntegerFactorial/src/bigintegerfactorial/BigIntegerFactorial.java
@@ -13,31 +13,24 @@ import java.util.Scanner;
  */
 public class BigIntegerFactorial {
 
- 
-    
     public static void main(String[] args) {
-       
-        
- Scanner s =new Scanner(System.in);
- 
-       
-        System.out.println("limit");
-       
-       int userInput=s.nextInt();
-        
-     
-        BigInteger factorial=BigInteger.ONE;
-        
-         for(int i = 1; i <= userInput; i++){
-             
-   factorial= factorial.multiply(BigInteger.valueOf(i));
 
-         }
+        Scanner s = new Scanner(System.in);
 
+        System.out.println("Enter Number");
+
+        int userInput = s.nextInt();
+//        BigInteger factorial= new BigInteger("10");
+//      BigInteger factorial= BigInteger.valueOf("10");
+        BigInteger factorial = BigInteger.ONE;
+
+        for (int i = 1; i <= userInput; i++) {
+
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
         System.out.println(factorial);
-
 
         
     }
-    
+
 }
