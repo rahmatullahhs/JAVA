3aabb9145d25a650d9d8caf5cf9c2c7093700379
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-26 16:59:43 +0600

    add

--- arrayPractice/src/arraypractice/ArrayPractice.java
+++ arrayPractice/src/arraypractice/ArrayPractice.java
@@ -1,15 +1,13 @@
-
 package arraypractice;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayPractice {
 
-
     public static void main(String[] args) {
-        
-        Scanner s =new Scanner(System.in);
+
+        Scanner s = new Scanner(System.in);
+
 //        String[] names=new String[14]; (names array)
 //        
 //        int[]marks=new int[14];
@@ -23,8 +21,6 @@ public class ArrayPractice {
 //        System.out.println(Arrays.toString(names));
 //        
 //       ------------------------- 
-        
-         
 //      String[] names={"Rahmat","Easin","Parvaz","Rakib","sadier"};
 //      
 //        int[]marks={44,55,66,77,88,99};
@@ -39,12 +35,9 @@ public class ArrayPractice {
 //         list[4]="my name is sadier and mark 22";
 //        
 //         System.out.println(Arrays.toString(list)+ "\n");
-         
 //         index=0 but name=1
 //         for(int index=0; index<names.length; index++){
 //             System.out.println("My name is " + names[index]+ "mark " +marks[index] );
-         
-             
 //          System.out.println("Enter count");
 //          int n =s.nextInt();
 //            String[] names = new String[n];
@@ -56,61 +49,32 @@ public class ArrayPractice {
 //         
 //              System.out.println("Enter Marks");
 //        int Marks=s.nextInt();
-         
+        String[] names = {"Rahmat", "Easin", "Parvaz", "Rakib", "sadier"};
 
+        int[] passwords = {111, 222, 333, 444, 555, 666};
 
-String[] names={"Rahmat","Easin","Parvaz","Rakib","sadier"};
-
- int[] passwords={111,222,333,444,555,666};
-
-String userName=s.next();
+        String userName = s.next();
 
         for (int i = 0; i < names.length; i++) {
-            
+
             if (userName.equalsIgnoreCase(names[i])) {
-              
+
                 System.out.println("Enter your password");
-                
-             int password;
+
+                int password;
                 password = s.nextInt();
                 int index = i;
 
-          
                 if (password == passwords[index]) {
-                    System.out.println(" Welcome"  + userName);
-                }
-                else {   
+                    System.out.println(" Welcome" + userName);
+                } else {
                     System.out.println("Password is incorrect");
                 }
-                   
-                }
-                break;
-
 
             }
-      
+            break;
+        }
 
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-             }
-             
-     
-//        -----------
     }
+
+}
--- positiveNegZero/src/positivenegzero/PositiveNegZero.java
+++ positiveNegZero/src/positivenegzero/PositiveNegZero.java
@@ -1,22 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package positivenegzero;
 
 import java.util.Scanner;
 
-/**
- *
- * @author saruk
- */
 public class PositiveNegZero {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
 
         Scanner s = new Scanner(System.in);
         System.out.println("enter your number");
@@ -28,113 +16,8 @@ public class PositiveNegZero {
             System.out.println("zero");
         } else {
             System.out.println("negative");
+ }
 
-        }
-
-        
-        
-        
-        
-        
-        
-        
-//          System.out.println("enter your number");
-//        int input = s.nextInt();
-//
-//        if (input > 0) {
-//            System.out.println("positive");
-//        } else if (input == 0) {
-//            System.out.println("zero");
-//        } else {
-//            System.out.println("negative");
-//
-//        }
-//
-//        
-//        
-//                System.out.println("enter your number");
-//        int input = s.nextInt();
-//
-//        if (input > 0) {
-//            System.out.println("positive");
-//        } else if (input == 0) {
-//            System.out.println("zero");
-//        } else {
-//            System.out.println("negative");
-//
-//        }
-//
-//        
-//                System.out.println("enter your number");
-//        int input = s.nextInt();
-//
-//        if (input > 0) {
-//            System.out.println("positive");
-//        } else if (input == 0) {
-//            System.out.println("zero");
-//        } else {
-//            System.out.println("negative");
-//
-//        }
-//
-//        
-//                System.out.println("enter your number");
-//        int input = s.nextInt();
-//
-//        if (input > 0) {
-//            System.out.println("positive");
-//        } else if (input == 0) {
-//            System.out.println("zero");
-//        } else {
-//            System.out.println("negative");
-//
-//        }
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
 
 }
