c564c52267b903923ebcda74bbb8a0b9537d23ff
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-26 13:21:04 +0600

    add

--- ForLoop/src/forloop/ForLoop.java
+++ ForLoop/src/forloop/ForLoop.java
@@ -1,6 +1,5 @@
 package forloop;
 
-import com.sun.source.tree.ContinueTree;
 import java.util.Scanner;
 
 public class ForLoop {
@@ -8,56 +7,27 @@ public class ForLoop {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
+//prime number
+        System.out.println("enter number");
+        int input = s.nextInt();
 
-//        System.out.println("enter number");
-//        int input = s.nextInt();
-//
-//        int count = 0;
-//        for (int i = 1; i <= input; i++) {
-//            if (input % i == 0) {
-//                count++;
-//            }
-//        }
-//        if (count == 2) {
-//            System.out.println(input + " prime");
-//        } else {
-//            System.out.println("not prime");
-//        }
-<<<<<<< HEAD
-
-//fix value
-=======
-        ////fix value
->>>>>>> 01d74133c0fe65f90aea1423dd64d709af1da6c5
-//        int targetValue = 50;
-//        int counter = 1;
-//
-//        while (counter <= targetValue) {
-//            if (counter % 3 == 0 && counter % 5 == 0) {
-//
-//                counter++;
-//                continue;
-//            }
-//            System.out.println(counter);
-//            counter++;
-//
-//        }
-//
-//        int counterr = 0;
-//        while (counterr < targetValue) {
-//
-//            if (counterr <= targetValue) {
-//                counter++;
-//                System.out.println(counterr);
-//            }
-//
-//        }
-//        counter++;
-
-        int userInput = s.nextInt();
-        for (int i = 1; i <= userInput; i++) {
-            System.out.println(i + " do it again");
+        int count = 0;
+        for (int i = 1; i <= input; i++) {
+            if (input % i == 0) {
+                count++;
+            }
         }
+        if (count == 1) {
+            System.out.println(input + " prime");
+        } else {
+            System.out.println("not prime");
+        }
+//number sequence
+//System.out.println("how much sequence you want to see");
+//        int userInput = s.nextInt();
+//        for (int i = 1; i <= userInput; i++) {
+//            System.out.println(i + " sequence");
+//        }
 
     }
 
