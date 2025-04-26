4df90201b47e0644fa4f2c93d47a01270073aeaf
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-26 23:08:11 +0600

    add

--- TimesTable/src/timestable/TimesTable.java
+++ TimesTable/src/timestable/TimesTable.java
@@ -1,27 +1,34 @@
-
-
 package timestable;
 
 import java.util.Scanner;
 
-
 public class TimesTable {
 
-  
     public static void main(String[] args) {
 
-        Scanner s =new Scanner(System.in);
-        
-       
-      int userInput=s.nextInt();
-        
-        for (int i = 1; i <=10; i++) {
-            int result = userInput*i;
-            
-            System.out.println(userInput+"*"+i+"="+ result);
-            
+        Scanner s = new Scanner(System.in);
+
+//        
+//        for (int i = 1; i <= 10; i++) {
+//            int result = userInput * i;
+//            System.out.println(userInput + "*" + i + "=" + result);
+//
+//        }
+        int sum = 0;
+
+        for (;;) {
+            System.out.println("Enter  number");
+            int userInput = s.nextInt();
+
+            if (userInput < 0) {
+                System.out.println("enter positive intger");
+                break;
+            }
+            sum += userInput;
+
         }
+        System.out.println(sum+"reza");
 
     }
-    
+
 }
