ad15e3522097c6c67e0ed8236c25bfa7fb2b0849
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-28 17:03:29 +0600

    add

---
+++ ReadingDataUsingScanner/src/readingdatausingscanner/ReadingDataUsingScanner.java
@@ -0,0 +1,46 @@
+
+package readingdatausingscanner;
+
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.MalformedInputException;
+import java.util.Scanner;
+
+
+public class ReadingDataUsingScanner {
+
+
+    public static void main(String[] args) {
+   
+        
+        try {
+           URL url = new URL("http://www.google.com/index.html");;
+            Scanner s =new Scanner(url.openStream());
+
+            
+        } catch (MalformedInputException ex) {
+            System.Logger.
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
--- learnFilClass/src/learnfilclass/LearnFilClass.java
+++ learnFilClass/src/learnfilclass/LearnFilClass.java
@@ -12,9 +12,12 @@ public class LearnFilClass {
     public static void main(String[] args) throws FileNotFoundException {
 
         File f = new File("c://Users//Admin//Desktop//even.txt");
-        
+        java.io.File file = new java.io.File("scores.txt");
        
-        
+ if (file.exists()) {
+        System.out.println("File already exists");
+        System.exit(1);
+ }
         try (PrintWriter pw = new PrintWriter(f)) {
             pw.print("My brain is not Braining");
         }
