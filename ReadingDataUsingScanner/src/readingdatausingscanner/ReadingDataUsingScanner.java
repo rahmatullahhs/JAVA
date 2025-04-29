dc54a8b8cf2bc5e9ebd4e0b2a75069b982cf35e8
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-04-29 15:27:26 +0600

    add

--- ReadingDataUsingScanner/src/readingdatausingscanner/ReadingDataUsingScanner.java
+++ ReadingDataUsingScanner/src/readingdatausingscanner/ReadingDataUsingScanner.java
@@ -1,12 +1,6 @@
 
 package readingdatausingscanner;
 
-import java.net.URI;
-import java.net.URL;
-import java.nio.charset.MalformedInputException;
-import java.util.Scanner;
-
-
 public class ReadingDataUsingScanner {
 
 
