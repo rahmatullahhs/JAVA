179e23c1d1d87b721aca862d648c7bfc082326b8
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-05-08 15:32:42 +0600

    add

--- 2dArray/src/pkg2darray/Main.java
+++ 2dArray/src/pkg2darray/Main.java
@@ -6,32 +6,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter row size");
-        int rowSize = s.nextInt();
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter col size");
-        int colSize = s.nextInt();
+        System.out.print("Enter your row limit: ");
+        int rowSize = scan.nextInt();
 
-        int[][] userArrays = new int[rowSize][colSize];
+        System.out.print("Enter your column limit: ");
+        int columnSize = scan.nextInt();
 
-        for (int row = 0; row < userArrays.length; rowSize++) {
+        int[][] myArrays = new int[rowSize][columnSize];
 
-            for (int col = 0; col < userArrays[rowSize].length; colSize++) {
-                System.out.println("enter value for row " + (rowSize + 1) + "colmon" + (colSize = 1) + ":");
+        for (int row = 0; row < myArrays.length; row++) {
 
-                userArrays[rowSize][colSize] = s.nextInt();
+            for (int column = 0; column < myArrays[row].length; column++) {
+                System.out.print("Enter value for row " + (row + 1) + " x column " + (column + 1) + ": ");
+                int userInput = scan.nextInt();
+                myArrays[row][column] = userInput;
             }
-
         }
 
         System.out.println("Your array is: ");
 
-        for (int[] userArray : userArrays) {
-            for (int col = 0; col < userArray.length; col++) {
-                System.out.print(userArray[col] + " ");
+        for (int[] myArray : myArrays) {
+            for (int column = 0; column < myArray.length; column++) {
+                System.out.print(myArray[column] + " ");
             }
-            System.out.println("\n");
+            System.out.println("");
         }
 
     }
