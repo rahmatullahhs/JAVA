fcf1b5cea5bc0a59582feff2852c077239b679ae
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-05-11 19:16:29 +0600

    add

--- ShapeArray/src/shapearray/ShapeArray.java
+++ ShapeArray/src/shapearray/ShapeArray.java
@@ -1,17 +1,17 @@
-
 package shapearray;
 
-/**
- *
- * @author saruk
- */
 public class ShapeArray {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+
+Circle c =new Circle(5);
+        System.out.println(c.area());
+
+
+
+
+
     }
-    
+
 }
